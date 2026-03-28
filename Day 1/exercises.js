35b12020ce213fb0772bf9531abf13b66a36e4e4
ansonnchan <ananryry180@gmail.com>
2026-03-28 10:36:01 -0700

    Day 1 Completed

---
+++ Day 1/exercises.js
@@ -0,0 +1,16 @@
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+
+/*
+Comments can make code readable,
+easy to reuse and informative
+*/
+
+let myAge;
+let yourAge;
+
+myAge = 25
+yourAge = 30
+
+console.log('I am' + myAge + 'years old.')
+console.log('You are' + yourAge + 'years old.')
---
+++ Day 1/test.js
@@ -0,0 +1,10 @@
+
+// let firstName = 'Anson'
+// let lastName = 'Chan'
+// let country = 'Hong Kong'
+// let initialSpeed = 40
+// const gravity = 9.81
+// initialSpeed = 40*gravity
+
+// console.log(firstName, lastName, country, initialSpeed, gravity)
+
