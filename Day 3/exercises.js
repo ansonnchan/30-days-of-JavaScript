f710183ae15b62b913703ac0773caab3b8d5eddb
ansonnchan <ananryry180@gmail.com>
2026-03-28 12:02:38 -0700

    Day 3 Completed

---
+++ Day 3/exercises.js
@@ -0,0 +1,26 @@
+// //exercise level 1 
+// const now = new Date()
+// let firstName = 'Anson', lastName = 'Chan', country = 'Canada', city = 'Vancouver', age = 19, isMarried = false, year = now.getFullYear()
+// console.log(typeof firstName)
+// console.log(typeof lastName)
+// console.log(typeof country)
+// console.log(typeof city)
+// console.log(typeof age)
+// console.log(typeof isMarried)
+// console.log(typeof year)
+
+// console.log('10' === 10)
+
+//Exercise 3 
+
+// const now = new Date ()
+// let year = now.getFullYear()
+// let month = now.getMonth()+1
+// let date = now.getDate()
+// let hour = now.getHours()
+// let minute = now.getMinutes()
+
+// console.log(year+'-'+month+'-'+date+ ' ' + hour + ':' + minute)
+
+//level 2
+let base = parseFloat(prompt('Enter base:')) 
---
+++ Day 3/test.js
@@ -0,0 +1,22 @@
+// const PI = Math.PI
+// let radius = 100
+// const area = PI*(radius**2)
+
+// console.log(`The area of a circle with radius ${radius} is ${area}`)
+
+// let number = 5
+
+// number > 0 ? console.log("The number is positive") : console.log("The number is false")
+
+// let number = prompt('Enter in a number') 
+//     console.log(number)
+// const now = new Date ()
+// console.log(now)
+// console.log(now.getFullYear())
+// console.log(now.getMonth())
+// console.log(now.getDate())
+// console.log(now.getDay())
+// console.log(now.getHours())
+
+// const now = new Date()
+// console.log(now.getTime())
