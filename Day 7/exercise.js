af99018eb142913899f660419495e85220176005
ansonnchan <ananryry180@gmail.com>
2026-03-28 21:58:32 -0700

    Day 7 Completed

---
+++ Day 7/exercise.js
@@ -0,0 +1,57 @@
+// function fullName (fullName) {
+//     console.log(fullName)
+// }
+
+// function fullName(firstName, lastName){
+//     console.log(firstName + ' ' + lastName)
+// }
+// fullName('Anson', 'Chan')
+
+// const areaofRectangle = (length, width) => {
+//     console.log(length*width)
+// }
+
+// areaofRectangle(2,3)
+
+// function checkSeason(month) {
+//     let winter = ['December', 'January', 'February']
+//     let spring = ['March', 'April', 'May']
+//     let summer = ['June', 'July', 'August']
+
+//     if (winter.includes(month)) {
+//         console.log('Winter')
+//     } else if (spring.includes(month)) {
+//         console.log('Spring')
+//     } else if (summer.includes(month)) {
+//         console.log('Summer')
+//     } else {
+//         console.log('Autumn')
+//     }
+// }
+
+// checkSeason('August')
+
+// function findMax(num1, num2, num3) {
+//     let arr = [num1, num2, num3]
+//     arr.sort()
+//     console.log(arr[2])
+// } 
+
+// findMax(-1,15,0)
+
+// function solveQuadEquation(a, b, c) {
+//     let root1 = (-b + Math.sqrt(b*b - 4*a*c))/(2*a)
+//     let root2 = (-b - Math.sqrt(b*b - 4*a*c))/(2*a)
+//     console.log({root1,root2})
+// }
+
+// solveQuadEquation(1,4,4)
+
+// function printArray(arr) {
+//    for (const num of arr) {
+//     console.log(num)
+//    }
+// }
+
+// printArray([1,2,3,4,5,6,1,2,3,4,5,6])
+
---
+++ Day 7/test.js
@@ -0,0 +1,12 @@
+// function addTwoNumbers(a,b) {
+//     return a + b
+// }
+
+// console.log(addTwoNumbers(69,67))
+
+// const areaOfCircle = (radius) => {
+//     let area = Math.PI*radius*radius
+//     return area
+// }
+// console.log(areaOfCircle(10))
+
