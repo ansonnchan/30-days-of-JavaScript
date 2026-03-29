665e6a2701d763dcb2443b49a87ef595719cc6b8
ansonnchan <ananryry180@gmail.com>
2026-03-29 15:33:32 -0700

    Day 10 Completed

---
+++ Day 10/exercise.js
@@ -0,0 +1,15 @@
+// const emptySet = new Set ()
+// const newSet = new Set ()
+
+// for (let i = 0; i <= 10; i++) {
+//     newSet.add(i)
+// }
+
+// console.log(newSet)
+// newSet.delete(10)
+// console.log(newSet)
+
+// const newMap = new Map ()
+// const strArray = ['apple','banana','apple','date','fig','apple'];
+// const strSet = new Set(strArray.slice(0,4));
+// console.log(strSet);
---
+++ Day 10/test.js
@@ -0,0 +1,37 @@
+// const companies = new Set()
+// console.log(companies) => empty set
+
+// const languages = ['English', 'Chinese', ' Finnish', 'Japanese', 'Korean', 'English', 'Finnish', 'Japanese', 'Korean']
+// const setOfLanguages = new Set(languages)
+// // console.log(setOfLanguages)
+
+// for (const language of setOfLanguages) {
+//     if (language[0] == 'C') console.log(language)
+// }
+
+// const companies = new Set ()
+// console.log(companies.size)
+// companies.add('Google')
+// companies.add('Amazon')
+// companies.add('Microsoft')
+// console.log(companies, companies.size)
+
+// let a = [1,2,3,4,5]
+// let b = [2,3,4]
+// let c = [... a, ... b]
+
+// let A = new Set(a)
+// let B = new Set(b)
+// let C = new Set(c)
+// console.log(C)
+
+// let a = [1,2,3,4,5]
+// let b = [5,6,7,8,9]
+
+// let A = new Set(a)
+// let B = new Set(b)
+
+// let c = a.filter((num) => B.has(num))
+// let C = new Set(c)
+
+// console.log(C)
