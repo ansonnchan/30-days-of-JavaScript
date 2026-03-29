b9b825aefb2eab625cb0ee6225b82ba80c0e19e6
ansonnchan <ananryry180@gmail.com>
2026-03-29 15:16:09 -0700

    Day 9 Completed

---
+++ Day 9/exercise.js
@@ -0,0 +1,32 @@
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
+
+// countries.forEach((country) => console.log(country))
+// console.log(countries.map((country) => (country.toUpperCase())
+// ))
+// console.log(numbers.map((number) => (number*number)))
+
+// console.log(products.map((product) => (product.price)))
+
+// console.log(countries.filter((country) => !country.includes('Land')))
+// console.log(countries.filter((country) => !(country.length == 6)))
+// console.log(countries.filter((country) => !(country[0] == 'F')))
+
+// function getStringLists (arr) {
+//     return arr.filter((element) => (typeof (element) == 'string'))
+// }
+
+// let arr = [1,2,3, 'hello', 'bruh', true, false]
+// console.log(getStringLists(arr))
+
+// console.log(numbers.reduce((acc, x) => acc+x, 0))
+
---
+++ Day 9/test.js
@@ -0,0 +1,37 @@
+// const callback = (n) => {
+//     return n **2
+// }
+
+// console.log(callback(4))
+
+// function cube (callback, n) {
+//     return callback(n)*n
+// }
+// console.log(cube(callback, 3))
+
+// const numbers = [1,2,3,4,5]
+// const sumArray = (arr) => {
+//     let sum = 0
+//     const callback = function(element) {
+//         sum += element
+//     }
+//     arr.forEach(callback)
+//     return sum
+// }
+// console.log(sumArray(numbers))
+
+// sayHello = () => {
+//     console.log('Hello')
+// }
+
+// setInterval(sayHello, 1000)
+
+// const numbers = [1,2,3,4,5]
+
+// const numberSquare = numbers.map((num) => num*num)
+// console.log(numberSquare)
+
+// const shoppingPrices = [10,30,32,40,30,15,20]
+// const filterShop = shoppingPrices.filter((price) => price<30)
+// console.log(filterShop)'
+
