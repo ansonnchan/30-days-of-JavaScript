343308bbe48241acb0bd4357f08e0f0c6ae4f94b
ansonnchan <ananryry180@gmail.com>
2026-03-29 16:11:49 -0700

    Day 12 Completed

---
+++ Day 12/exercise.js
@@ -0,0 +1,17 @@
+// const pattern  = /\d+/g
+// const txt = 'He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per match'
+// const numbers = txt.match(pattern).map(Number); 
+// console.log(numbers)
+
+// const annualSalary = numbers[0] *12
+// const annualBonus = numbers[1]
+// const annualCourses = numbers[2]*12
+
+// const totalIncome = annualSalary + annualBonus + annualCourses;
+// console.log(totalIncome)
+
+// const points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8']
+// const sortedPoints = points.map(Number).sort((a,b) => a-b)
+// distance = sortedPoints[sortedPoints.length - 1] - sortedPoints[0]
+// console.log(distance)
+
---
+++ Day 12/test.js
@@ -0,0 +1,27 @@
+// const str = 'I Love JavaScript'
+// const pattern = /love/g
+// const result = pattern.test(str)
+// console.log(result)
+
+
+// const str = 'I Love JavaScript' 
+// const pattern = /love/g
+// const result = str.match(pattern)
+// console.log(result)
+
+// const pattern = /love/g
+// const result = str.search(pattern)
+// console.log(result)
+
+// const txt = 'Python is the most beautiful language that a human begin has ever created.\
+// I recommend python for a first programming language'
+
+// let matchReplaced = txt.replace(/Python|python/, 'JavaScript')
+// console.log(matchReplaced)
+
+// const pattern = '[Aa]pple'
+// const txt = 'Apple and banana are fruits. An old cliche says an apple a day keeps the doctor away has been replaced by\
+// a banana a dau keeps the doctor far far away.'
+// const matches = txt.match(pattern)
+// console.log(matches)
+
