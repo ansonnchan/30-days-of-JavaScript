dcfe8dcc79b9cef3302530ba67a789247a1e7cd9
ansonnchan <ananryry180@gmail.com>
2026-03-29 16:20:22 -0700

    Day 13 Completed

---
+++ Day 13/test.js
@@ -0,0 +1,117 @@
+// // console.log('30 Days of JavaScript')
+// // console.log('%d %s of JavaScript', 30, 'Days')
+// const names = ['Anson', 'Chan']
+// console.table(names)
+// const users = [
+//   {
+//     name: 'Asabeneh',
+//     title: 'Programmer',
+//     country: 'Finland',
+//     city: 'Helsinki',
+//     age: 250
+//   },
+//   {
+//     name: 'Eyob',
+//     title: 'Teacher',
+//     country: 'Sweden',
+//     city: 'London',
+//     age: 25
+//   },
+//   {
+//     name: 'Asab',
+//     title: 'Instructor',
+//     country: 'Norway',
+//     city: 'Oslo',
+//     age: 22
+//   },
+//   {
+//     name: 'Matias',
+//     title: 'Developer',
+//     country: 'Denmark',
+//     city: 'Copenhagen',
+//     age: 28
+//   }
+// ]
+// console.table(users)
+
+// const countries = [
+//   ['Finland', 'Helsinki'],
+//   ['Sweden', 'Stockholm'],
+//   ['Norway', 'Oslo']
+// ]
+
+// console.time('Regular for loop')
+// for (let i = 0; i < countries.length; i++) {
+//   console.log(countries[i][0], countries[i][1])
+// }
+// console.timeEnd('Regular for loop')
+
+// console.time('for of loop')
+// for (const [name, city] of countries) {
+//   console.log(name, city)
+// }
+// console.timeEnd('for of loop')
+
+// console.time('forEach loop')
+// countries.forEach(([name, city]) => {
+//   console.log(name, city)
+// })
+// console.timeEnd('forEach loop')
+
+// const names = ['Asabeneh', 'Brook', 'David', 'John']
+// const countries = [
+//   ['Finland', 'Helsinki'],
+//   ['Sweden', 'Stockholm'],
+//   ['Norway', 'Oslo']
+// ]
+// const user = {
+//   name: 'Asabeneh',
+//   title: 'Programmer',
+//   country: 'Finland',
+//   city: 'Helsinki',
+//   age: 250
+// }
+// const users = [
+//   {
+//     name: 'Asabeneh',
+//     title: 'Programmer',
+//     country: 'Finland',
+//     city: 'Helsinki',
+//     age: 250
+//   },
+//   {
+//     name: 'Eyob',
+//     title: 'Teacher',
+//     country: 'Sweden',
+//     city: 'London',
+//     age: 25
+//   },
+//   {
+//     name: 'Asab',
+//     title: 'Instructor',
+//     country: 'Norway',
+//     city: 'Oslo',
+//     age: 22
+//   },
+//   {
+//     name: 'Matias',
+//     title: 'Developer',
+//     country: 'Denmark',
+//     city: 'Copenhagen',
+//     age: 28
+//   }
+// ]
+
+// console.group('Names')
+// console.log(names)
+// console.groupEnd()
+
+// console.group('Countries')
+// console.log(countries)
+// console.groupEnd()
+
+// console.group('Users')
+// console.log(user)
+// console.log(users)
+// console.groupEnd()
+
