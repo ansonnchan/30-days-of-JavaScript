26a759a0e90acede61941f24db08d144185fe581
ansonnchan <ananryry180@gmail.com>
2026-03-28 21:30:18 -0700

    Day 6 Completed

---
+++ Day 6/exercise.js
@@ -0,0 +1,38 @@
+// for (let i = 1; i < 8; i++) {
+//     console.log('#'.repeat(i))
+// }
+
+// for (let i = 0; i < 11; i++) {
+//     let total = i*i
+//     console.log(`${i} x ${i} = ${total}`)
+// }
+
+// console.log('i\ti^2\t i^3')
+// for (let i = 0; i < 11; i++) {
+//     console.log(`${i}\t${i*i}\t ${i*i*i}`)
+// }
+
+// for (let i = 0; i <= 100; i++) {
+//     if (i%2==0) {
+//         console.log(i)
+//     }
+// }
+
+// let sum = 0
+// for (let i = 0; i <= 100; i++) {
+//     sum += i;
+// }
+// console.log(sum)
+
+// let evenSum = 0
+// let oddSum = 0
+
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//         evenSum += i
+//     } else {
+//         oddSum += i
+//     }
+// }
+// console.log(evenSum)
+// console.log(oddSum)
