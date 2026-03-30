38598726e0d77f62d3c3caa4cb878b56d456c549
ansonnchan <ananryry180@gmail.com>
2026-03-30 13:36:11 -0700

    Day 20 Completed

---
+++ Day 20/test.js
@@ -0,0 +1,12 @@
+const hexaColor = () => {
+    const str = '0123456789abcdef'
+    let hexa = '#'
+    let index
+    for (let i = 0; i < 6; i++) {
+        index = Math.floor(Math.random()*str.length)
+        hexa += str[index]
+    }
+    return hexa
+}
+
+console.log(hexaColor())
